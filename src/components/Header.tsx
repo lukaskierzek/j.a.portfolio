a4eafde1fb29818487d5edbff3e839bfb16599a9
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-08 16:18:17 +0100

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import header_image from './Images/header_image.jpg';
+
+function Header() {
+    return (
+        <>
+            <img src={header_image} alt="information_about_justyna_adamus"/>
+        </>
+    );
+}
+
+export default Header;

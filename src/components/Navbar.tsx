9cd56230f63a39296e63b87bfcfdb85a7039d6a8
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-08 16:18:48 +0100

    Create Navbar.tsx with navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <>
+            <nav><a href="#books">Książki</a> | <a href="#other">Inne</a> | <a href="#contact">Kontakt</a></nav>
+        </>
+    );
+}
+
+export default Navbar;

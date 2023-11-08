dcf55d7b7ef718cc6c1c3243799a7937cc188719
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-08 16:18:02 +0100

    Create Footer.tsx with footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    const link: string = "#"
+    const author: string = "Łukasz Kierzek"
+    return (
+        <>
+            <footer>
+                Autor: {author}. Wszelkie prawa zastrzeżone
+            </footer>
+        </>
+    );
+}
+
+export default Footer;

1776f669851ed7f877414afe1dcef7784f2963cd
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-10 20:26:13 +0100

    Refactor Books.tsx with Material UI

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,18 +1,60 @@
 import {BookList} from "./Data/BookList";
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import {CardContent, CardMedia, Typography} from "@mui/material";
 
 function Books() {
 
     const BooksItems = BookList.map(book =>
         <>
-            <li>
-                {book.title}<br/>{book.author}<br />{book.workType}<br />{book.imgUrl}<br />{book.about}
-            </li>
+            <Card sx={{display: 'flex', marginBottom: '25px', padding: '10px'}}>
+                <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                    <CardContent>
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                fontStyle: 'italic',
+                                fontWeight: 'bold'
+                            }}
+                        >
+                            {book.title}
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                fontWeight: 'bold'
+                            }}
+                        >
+                            {book.author}
+                        </Typography>
+                        <Box component="div" sx={{
+                            backgroundColor: "Blue",
+                            height: "20px",
+                            width: "130px"
+                        }}></Box>
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                fontWeight: 'bold'
+                            }}
+                        >
+                            {book.workType}
+                        </Typography>
+                        <Typography variant="body1" color="text.primary">
+                            {book.about}
+                        </Typography>
+                    </CardContent>
+                </Box>
+                <CardMedia
+                    component="img"
+                    image={book.imgUrl}
+                />
+            </Card>
         </>
     )
     return (
         <>
-            <p>KSIĄŻKI</p>
-            <ul>{BooksItems}</ul>
+            {BooksItems}
         </>
     );
 }

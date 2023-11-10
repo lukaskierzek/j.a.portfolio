a2b221a7ad6e8028cb5a6d8fbbebe4963c9024a2
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-10 20:25:48 +0100

    Add img to box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import header_image from './Images/header_image.jpg';
+import Box from "@mui/material/Box";
 
 function Header() {
     return (
         <>
-            <img src={header_image} alt="information_about_justyna_adamus"/>
+            <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                <img src={header_image} alt="information_about_justyna_adamus"/>
+
+            </Box>
+
         </>
     );
 }

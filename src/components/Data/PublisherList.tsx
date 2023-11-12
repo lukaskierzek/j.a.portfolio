58e713da68704a44c5abfdbf6013b080ed2322cc
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-12 18:04:28 +0100

    Add PublisherList.tsx file

---
+++ src/components/Data/PublisherList.tsx
@@ -0,0 +1,11 @@
+interface IPublisherList {
+    publisherIX: string,
+    publisherHaart: string
+}
+
+const publisherList: IPublisherList = {
+    publisherIX: "Wydawnictwa IX",
+    publisherHaart: "Wydawnictwa Ha!art"
+}
+
+export default publisherList;

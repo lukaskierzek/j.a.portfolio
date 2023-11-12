750cbf6b902d7463534bd93890ec6eaa15cb5c23
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-12 17:59:30 +0100

    Refactor Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,45 @@
+import {Grid, Stack} from "@mui/material";
+import Button from '@mui/material/Button';
+
+export enum ChapterPL {
+    BOOKS = 'Książki',
+    OTHER = 'Inne',
+    CONTACT = 'Kontakt',
+}
+
+export enum ChapterENG {
+    BOOKS = 'Books',
+    OTHER = 'Other',
+    CONTACT = 'Contact',
+}
+
 function Navbar() {
+    function linkToChapter(href: any, name: string) {
+        return (
+            <Button>
+                <a
+                    href={'#' + href}
+                    style={{
+                        padding: '10px'
+                    }}
+                >
+                    {name}
+                </a>
+            </Button>
+        );
+    }
+
     return (
         <>
-            <nav><a href="#books">Książki</a> | <a href="#other">Inne</a> | <a href="#contact">Kontakt</a></nav>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Stack direction="row">
+                        {linkToChapter(ChapterENG.BOOKS, ChapterPL.BOOKS)}
+                        {linkToChapter(ChapterENG.OTHER, ChapterPL.OTHER)}
+                        {linkToChapter(ChapterENG.CONTACT, ChapterPL.CONTACT)}
+                    </Stack>
+                </Grid>
+            </Grid>
         </>
     );
 }

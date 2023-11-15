f306f2387ed2851ecc43d058053fe5b5a87240d9
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-15 22:07:01 +0100

    Modify Other.tsx
    Add code for display OtherList.tsx

--- src/components/Other.tsx
+++ src/components/Other.tsx
@@ -1,10 +1,53 @@
 import Topic from "./Topic";
 import {ChapterENG, ChapterPL} from "./Navbar";
+import {CardContent, CardMedia, Typography} from "@mui/material";
+import OtherList from "./Data/OtherList";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+
 
 function Other() {
+
+    const OtherItems = OtherList.map(other =>
+        <>
+            <Card sx={{display: 'flex', marginBottom: '25px', padding: '10px'}}>
+                <Box sx={{marginBottom: '25px', padding: '10px'}}>
+                    <CardContent>
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                fontWeight: 'bold',
+                                padding: '0 0 10px 0'
+                            }}
+                            align="center"
+                        >
+                            {other.title}
+                        </Typography>
+                        <Typography
+                            variant="body1"
+                            color="text.primary"
+                        >
+                            {other.about}
+                        </Typography>
+                    </CardContent>
+                </Box>
+                <CardMedia
+                    component="img"
+                    image={other.img}
+                    sx={{
+                        width: "40%",
+                        height: "40%",
+                    }}
+                    alt={'test'}
+                />
+            </Card>
+        </>
+    )
+
     return (
         <>
             {Topic(ChapterPL.OTHER, ChapterENG.OTHER)}
+            {OtherItems}
         </>
     );
 }

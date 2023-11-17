3528e2047cf5aa10785b17421ad2dbc02af09f59
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-17 17:08:09 +0100

    Modify Styles.tsx
    Add style for Book and Other Cards

--- src/components/Data/Styles.tsx
+++ src/components/Data/Styles.tsx
@@ -6,3 +6,11 @@ export const styleIB = {
 export const styleB = {
     fontWeight: 'bold'
 }
+
+export const styleCardBO = {
+    display: 'flex',
+    marginBottom: '25px',
+    padding: '10px',
+    backgroundColor: "#22A39F",
+    backgroundImage: "linear-gradient(to right, #E0EEEF, #56BEB7)",
+}

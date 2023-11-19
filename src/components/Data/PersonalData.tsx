4971419fa41e44f49cc8f1880211bfb90bec68ef
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-19 19:08:07 +0100

    Create PersonalData.tsx
    Add information about JA and LK

---
+++ src/components/Data/PersonalData.tsx
@@ -0,0 +1,8 @@
+enum PersonalData {
+    JA_FULL_NAME= "Justyna Adamus",
+    JA_EMAIL = "justyna.adamus9@gmail.com",
+    JA_PHONE_NUMBER = "+48 535 384 840",
+    LK_FULL_NAME = "Łukasz Kierzek",
+}
+
+export default PersonalData;

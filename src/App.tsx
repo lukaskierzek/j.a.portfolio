0d25c0eaa7b5804159081ca636485b44048bbae6
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-22 10:55:49 +0100

    Refactor App.tsx imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Books from "./components/Books";
-import Other from "./components/Other";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Books from "./components/Books/Books";
+import Other from "./components/Other/Other";
+import Footer from "./components/Footer/Footer";
 import Container from '@mui/material/Container';
 
 function App() {

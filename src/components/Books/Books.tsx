2a5c1e100e47c9d58d0a62b5a709ae688420f7a8
lukaskierzek <lukas.kierzek@gmail.com>
2023-11-25 14:16:25 +0100

    Modify Books and Other
    Change box to grids

--- src/components/Books/Books.tsx
+++ src/components/Books/Books.tsx
@@ -1,72 +1,68 @@
 import {BookList} from "./BookList";
 import Card from '@mui/material/Card';
-import Box from '@mui/material/Box';
 import {CardContent, CardMedia, Typography} from "@mui/material";
 import Topic from "../Topic/Topic";
 import {ChapterENG, ChapterPL} from "../Data/Chapters";
 import {styleCardBO} from "../Data/Styles";
 import React from "react";
+import Grid from "@mui/material/Grid";
 
 export default function Books() {
 
     const BooksItems = BookList.map(book =>
         <>
-            <Card sx={styleCardBO} key={book.title}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <CardContent>
-                        <Typography
-                            variant="h5"
-                            sx={{
-                                fontWeight: 'bold',
-                                padding: '0 0 10px 0'
-                            }}
-                        >
-                            {book.author}
-                        </Typography>
-                        <Typography
-                            variant="h4"
-                            sx={{
-                                fontWeight: 'bold',
-                                color: 'green'
-                            }}
-                        >
-                            {book.title}
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            sx={{
-                                fontWeight: 'bold',
-                                padding: '20px 0 20px 0'
-                            }}
-                        >
-                            {book.workType}
-                        </Typography>
-                        <Typography
-                            variant="body1"
-                            color="text.primary"
-                        >
-                            {book.about}
-                        </Typography>
-                    </CardContent>
-                </Box>
-                <CardMedia
-                    component="img"
-                    image={book.imgUrl}
-                    sx={{
-                        width: "40%",
-                        height: "40%",
-                    }}
-                    alt={`Okładka książki ${book.title}`}
-                    loading="lazy"
-                />
+            <Card sx={styleCardBO} key={`${book.title}${book.id}`}>
+                <Grid container>
+                    <Grid lg={8}>
+                        <CardContent>
+                            <Typography
+                                variant="h5"
+                                sx={{
+                                    fontWeight: 'bold',
+                                    padding: '0 0 10px 0'
+                                }}
+                            >
+                                {book.author}
+                            </Typography>
+                            <Typography
+                                variant="h4"
+                                sx={{
+                                    fontWeight: 'bold',
+                                    color: 'green'
+                                }}
+                            >
+                                {book.title}
+                            </Typography>
+                            <Typography
+                                variant="h5"
+                                sx={{
+                                    fontWeight: 'bold',
+                                    padding: '20px 0 20px 0'
+                                }}
+                            >
+                                {book.workType}
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                color="text.primary"
+                            >
+                                {book.about}
+                            </Typography>
+                        </CardContent>
+                    </Grid>
+                    <Grid lg={4}>
+                        <CardMedia
+                            component="img"
+                            image={book.imgUrl}
+                            alt={`Okładka książki ${book.title}`}
+                            loading="lazy"
+                        />
+                    </Grid>
+                </Grid>
             </Card>
         </>
     )
+
     return (
         <>
             {Topic(ChapterPL.BOOKS, ChapterENG.BOOKS)}
--- src/components/Other/Other.tsx
+++ src/components/Other/Other.tsx
@@ -3,50 +3,44 @@ import {ChapterENG, ChapterPL} from "../Data/Chapters";
 import {CardContent, CardMedia, Typography} from "@mui/material";
 import OtherList from "./OtherList";
 import Card from "@mui/material/Card";
-import Box from "@mui/material/Box";
 import {styleCardBO} from "../Data/Styles";
-
+import Grid from "@mui/material/Grid";
 
 export default function Other() {
 
     const OtherItems = OtherList.map(other =>
         <>
             <Card sx={styleCardBO} key={other.title}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <CardContent>
-                        <Typography
-                            variant="h5"
-                            sx={{
-                                fontWeight: 'bold',
-                                padding: '0 0 10px 0'
-                            }}
-                            align="center"
-                        >
-                            {other.title}
-                        </Typography>
-                        <Typography
-                            variant="body1"
-                            color="text.primary"
-                        >
-                            {other.about}
-                        </Typography>
-                    </CardContent>
-                </Box>
-                <CardMedia
-                    component="img"
-                    image={other.img}
-                    sx={{
-                        width: "40%",
-                        height: "40%",
-                    }}
-                    alt={`Ikona ${other.title}`}
-                    loading="lazy"
-                />
+                <Grid container>
+                    <Grid lg={8}>
+                        <CardContent>
+                            <Typography
+                                variant="h5"
+                                sx={{
+                                    fontWeight: 'bold',
+                                    padding: '0 0 10px 0'
+                                }}
+                                align="center"
+                            >
+                                {other.title}
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                color="text.primary"
+                            >
+                                {other.about}
+                            </Typography>
+                        </CardContent>
+                    </Grid>
+                    <Grid lg={4}>
+                        <CardMedia
+                            component="img"
+                            image={other.img}
+                            alt={`Ikona ${other.title}`}
+                            loading="lazy"
+                        />
+                    </Grid>
+                </Grid>
             </Card>
         </>
     )

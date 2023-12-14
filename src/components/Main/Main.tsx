102431464e9c25147db271e8ca258750fccbcae5
lukaskierzek <lukas.kierzek@gmail.com>
2023-12-14 11:34:44 +0100

    Create Main.tsx file and add <Books/> and <Other/> to Main function

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import Books from "../Books/Books";
+import Other from "../Other/Other";
+
+export default function Main() {
+  return (
+    <main>
+      <Books/>
+      <Other/>
+    </main>
+  );
+}

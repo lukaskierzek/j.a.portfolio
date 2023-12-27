26ba718f5a97f675ed503cb56dab0de2eb8c8b6c
lukaskierzek <lukas.kierzek@gmail.com>
2023-12-27 20:12:57 +0100

    Add new book and refactor file

--- src/components/Books/BookList.tsx
+++ src/components/Books/BookList.tsx
@@ -320,7 +320,7 @@ export const BookList: IBook[] = [
           variant="body1"
           marginBottom={2}
         >
-          Podczas korekty tej publikacji zwracałam szczególną uwagę na ujednolicenie zapisów oraz  poprawność danych
+          Podczas korekty tej publikacji zwracałam szczególną uwagę na ujednolicenie zapisów oraz poprawność danych
           zawartych w końcowym wykazie filmów. Korekta poskładowa wiązała się natomiast z dokładnym sprawdzeniem
           hierarchii elementów niełatwego (ze względu na treść) składu.
         </Typography>
@@ -333,6 +333,44 @@ export const BookList: IBook[] = [
         </Typography>
       </>,
     imgUrl: "https://www.sklep.ha.art.pl/userdata/public/gfx/759/Haart_JaskulskiPawel_Dodatkifilmowe_okladka_20231212.jpg",
+  },
+  {
+    id: 10,
+    title: "Pomoc osobom pokrzywdzonym przestępstwem. Standaryzacja pracy pracowników służb pomocowych...",
+    author: "Praca zbiorowa",
+    workType: "Redakcja, korekta, korekta poskładowa",
+    about:
+      <>
+        <Typography
+          component="section"
+          variant="body1"
+          marginBottom={2}
+        >
+          <Typography component="span" fontWeight="bold">Podręcznik</Typography> jest dziełem wybitnych specjalistów,
+          którzy mają wieloletnie doświadczenie w zakresie pomocy pokrzywdzonym. Oprócz wyników badań zawiera wiele
+          merytorycznych treści: <Typography component="span" fontWeight="bold">wytyczne, analizy przypadków, schematy
+          postępowania</Typography>.
+        </Typography>
+        <Typography
+          component="section"
+          variant="body1"
+          marginBottom={2}
+        >
+          W ramach współpracy z firmą <Typography component="span" fontWeight="bold">AT ONCE</Typography> byłam
+          odpowiedzialna za redakcję, korektę oraz korektę poskładową publikacji. Ze względu na wielu autorów konieczne
+          było ujednolicenie poszczególnych zapisów (bibliografia, zapisy aktów prawnych) oraz zapoznanie się
+          z <Typography component="span" fontWeight="bold">terminologią prawniczą</Typography>.
+        </Typography>
+        <Typography
+          component="section"
+          variant="body1"
+        >
+          Książka dostępna jest w wersji papierowej oraz jako e-book do pobrania za darmo na
+          stronie {/*See comment about WYSOKIESTANDARDY in PublisherList.tsx*/}
+                  {returnLinkToBookstore("https://wysokiestandardy.pl/podrecznik/", Publisher.WYSOKIESTANDARDY)}.
+        </Typography>
+      </>,
+    imgUrl: "https://img.genial.ly/64cabab64f44770013cc346b/7a19d306-d744-49f2-95ba-c26e7beeca05.png",
   },
 // TODO: Add more books:)
 ]

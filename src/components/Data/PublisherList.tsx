99f2742cea2faafce434d11dddd14f883dbb89a2
lukaskierzek <lukas.kierzek@gmail.com>
2023-12-27 20:15:12 +0100

    Add new publisher

--- src/components/Data/PublisherList.tsx
+++ src/components/Data/PublisherList.tsx
@@ -1,6 +1,10 @@
 enum Publisher {
-  IX = "Wydawnictwa IX",
-  HAART = "Wydawnictwa Ha!art",
+  IX = "wydawnictwa IX",
+  HAART = "wydawnictwa Ha!art",
+  // 'wysokiestandardy' IS NOT A PUBLISHER.
+  // THIS IS THE WEBSITE FROM WHICH YOU CAN DOWNLOAD THE MANUAL.
+  // 'episteme' IS THE PUBLISHER.
+  WYSOKIESTANDARDY = "wysokiestandardy",
 }
 
 export default Publisher;
